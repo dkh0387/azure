64e7f285fd028b388a7ca57260ccbcd13a0e71d3
dkh0387 <deniskh87@gmail.com>
2025-01-07 14:45:43 +0100

    feat: chapter 78 completed incl. a web app for authentication and a console app for using Microsoft Graph API example (Azure Authentication)

---
+++ ConsoleAppUsageMicrosoftGraphAPI/GraphAPIConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Models;
+using System;
+using System.Threading.Tasks;
+
+
+
+namespace GraphAPIConsoleApp
+
+{
+
+    internal class Program
+
+    {
+
+        public async static Task Main(string[] args)
+        {
+            var scopes = new[] { "https://graph.microsoft.com/.default" };
+            var tenantId = "1fe56840-7d66-45e7-931e-3751441409b5";
+            var clientId = "c0f4b916-0fe6-4d63-b774-ff615dbdef6d";
+            var clientSecret = ".bL8Q~nA8S5eKGQxm~h6sdjkTLyuYhm~fDfI.b59";
+
+            var options = new TokenCredentialOptions
+            {
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+            };
+
+            var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, clientSecret, options);
+
+            var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+
+            UserCollectionResponse? users = await graphClient.Users.GetAsync();
+
+            foreach (var user in users.Value)
+            {
+                Console.WriteLine($"User DisplayName: {user.DisplayName}");
+            }
+
+        }
+
+    }
+
+}
