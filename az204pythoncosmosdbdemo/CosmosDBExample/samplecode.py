4ebccbef0f1f40d9558839d7935a8d42b12652a9
dkh0387 <deniskh87@gmail.com>
2025-06-13 10:52:08 +0200

    feat: cosmos db demo added rear operation

--- az204pythoncosmosdbdemo/CosmosDBExample/samplecode.py
+++ az204pythoncosmosdbdemo/CosmosDBExample/samplecode.py
@@ -1,6 +1,7 @@
 import os
 from azure.cosmos import CosmosClient, PartitionKey
 
+
 class PythonCosmosLab(object):
     my_connection_string = "AccountEndpoint=https://az204cosmosdb20250611.documents.azure.com:443/;AccountKey=l84vrvnTHGSFZNlLQ5rtg0SjBpETzlmkS6GFQIB3U8mLsoQn6FDg8XsmHZxpREuOYS96UF9rA53LACDbzOzXZg==;"
 
@@ -13,11 +14,25 @@ class PythonCosmosLab(object):
 
         my_container = my_database.create_container_if_not_exists("cars", partition_key_path)
 
-        generic_item = {'id': '3',
-                        'male': 'mercedes'
+        generic_item = {'id': '5',
+                        'male': 'jeep'
                         }
 
         my_container.create_item(body=generic_item)
+
+        query = "SELECT * FROM cars WHERE cars.male = @male"
+        parameters = [
+            {"name": "@male", "value": "lexus"}
+        ]
+
+        results = my_container.query_items(
+            query=query,
+            parameters=parameters,
+            enable_cross_partition_query=True
+        )
+
+        for item in results:
+            print(item)
 
 
 example = PythonCosmosLab()
